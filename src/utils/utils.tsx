d4cfa98e4a9b8582a42091b6a1fc82f1eff07dd5
Athina <athina.kantis@gmail.com>
2025-01-24 20:53:55 +0200

    update utils.tsx; add formatPhrase to ensure user input cannot be special characters, numeric values or whitespace

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,3 +1,16 @@
-export const isPalindrome = (phrase : string): boolean => {
+//
+// isPalindrome
+// Reverses the input and returns true if it matched the phrase
+// Otherwise, returns false
+export const isPalindrome = (phrase: string): boolean => {
     return phrase.split('').reverse().join('') === phrase;
 }
+
+
+//
+// validatePhrase
+// Replaces any numeric, special characters or whitespace
+// and returns the initial phrase to lowercase
+export const validatePhrase = (phrase: string): string => {
+    return phrase.replace(/[\d\s\W]+/g, '').trim().toLowerCase()
+}  
